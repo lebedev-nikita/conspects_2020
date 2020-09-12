dde8d85bb29c5f4007e369478f8dadc59ad16271
Nikita Lebedev <y87cgp@gmail.com>
2020-09-12 20:04:39 +0300

    сделал квадратичный

--- func_prog/1/new.scm
+++ func_prog/1/new.scm
@@ -1,20 +1,18 @@
 #lang scheme/base
 ; b
-; (define (2n-1!-list n) 
-;   (if (or (<= n 0) (not (integer? n)))
-;     '()
-;     (let fact )
+(define (fact3 n)
+  (if (<= n 0)
+    '()
+    (fact2 (- (* n 2) 1) '())
+  )
+)
 
-
-;   )
-; )
 (define (fact2 n lst) 
   (if (= n 0)
     lst
     (if (= 0 (modulo n 2))
-        (fact2 (- n 1) lst)
-        (fact2 (- n 1) (cons (fact n 1) lst)
-      )
+      (fact2 (- n 1) lst)
+      (fact2 (- n 1) (cons (fact n 1) lst))
     )
   )
 )
