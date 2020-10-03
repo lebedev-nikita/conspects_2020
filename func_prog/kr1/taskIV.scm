0119547e21889c24e9e91766c2e515a3ffae98c8
lebedev-nikita <y87cgp@gmail.com>
2020-10-03 21:20:36 +0300

    Работаю над четверотой

---
+++ func_prog/kr1/taskIV.scm
@@ -0,0 +1,64 @@
+#lang scheme/base
+
+(define empty-tree #())
+(define make-tree vector)
+(define tree? vector?)
+
+(define (tree-topleft tree) (vector-ref tree 0))
+(define (tree-topright tree) (vector-ref tree 1))
+(define (tree-botleft tree) (vector-ref tree 2))
+(define (tree-botright tree) (vector-ref tree 3))
+
+(define (taskIV t s cc)
+  (if (not (tree? t))
+    (* s t)
+    (+
+      (taskII (tree-topleft t) (/ s 4))
+      (taskII (tree-topright t) (/ s 4))
+      (taskII (tree-botleft t) (/ s 4))
+      (taskII (tree-botright t) (/ s 4))
+    )
+  )
+)
+
+
+(taskIV 
+  #(1 0 0 #(1 1 1 0)) 
+  16
+  (lambda (x s) (* s x))
+)
+
+
+; (lambda (w) 
+;   (cc (+ w
+;     (taskIV (tree-topright t) (/ s 4))
+;     (taskIV (tree-botleft t) (/ s 4))
+;     (taskIV (tree-botright t) (/ s 4))
+;   ))
+; )
+; (lambda (x) 
+;   (cc (+ w x
+;     (taskIV (tree-botleft t) (/ s 4))
+;     (taskIV (tree-botright t) (/ s 4))
+;   ))
+; )
+; (lambda (y) 
+;   (cc (+ w x y
+;     (taskIV (tree-botright t) (/ s 4))
+;   ))
+; )
+; (lambda (z) 
+;   (cc (+ w x y z)) 
+; )
+
+; (define (taskIV t s cc)
+;   (if (not (tree? t))
+;     (* s t)
+;     (+
+;       (taskII (tree-topleft t) (/ s 4))
+;       (taskII (tree-topright t) (/ s 4))
+;       (taskII (tree-botleft t) (/ s 4))
+;       (taskII (tree-botright t) (/ s 4))
+;     )
+;   )
+; )
