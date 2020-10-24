6ff997c30243f014087fe1692f1846db77ed6b98
lebedev-nikita <y87cgp@gmail.com>
2020-10-24 22:54:37 +0300

    autocommit

---
+++ func_prog/Vesna/template.scm
@@ -0,0 +1,25 @@
+#lang scheme/base
+
+(require scheme/mpair)
+(require math/number-theory)
+(require racket/hash)
+
+; (read-line)
+
+(define (parseStr str)
+  (define (addSpaces str)
+    (regexp-replace* #rx"([.!?-])" str " \\1 ")
+  )
+  (define (splitBySpaces str)
+    (regexp-split #rx" +" str)
+  )
+  (define (removeEmptyStr strLst)
+    (filter 
+      (lambda (str) (not (equal? str "")))
+      strLst
+    )
+  )
+  (removeEmptyStr (splitBySpaces (addSpaces str)))
+)
+
+(parseStr "Hello world! I am there! And I am the king!!!  ")
