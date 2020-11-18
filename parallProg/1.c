ad94b14b271dc383324805d70733a0b280c58018
lebedev-nikita <y87cgp@gmail.com>
2020-11-19 00:55:34 +0300

    пока не работает, но настроил грани таблицы

--- parallProg/1.c
+++ parallProg/1.c
@@ -18,26 +18,30 @@ int main(int argc, char **argv)
   MPI_Request req[2];
 
   // левый верхний угол
-  if (world_rank == 0) 
+  if (world_rank == 0)
   {
     char msg[MSG_LENGTH];
     for (int i = 0; i < MSG_LENGTH; i++)
     {
       msg[i] = 'a' + i % 26;
     }
-    for (int i = 0; i < NUM_CHUNKS; i++)
+    for (int i = 0; i < NUM_CHUNKS / 2; i++)
     {
-      MPI_Send(msg + i * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank + 1, i, MPI_COMM_WORLD);
+      MPI_Isend(msg + i * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank + 1, i, MPI_COMM_WORLD, &req[0]);
+      MPI_Isend(msg + (i + NUM_CHUNKS / 2) * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank + MATRIX_N, i + NUM_CHUNKS / 2, MPI_COMM_WORLD, &req[1]);
+      MPI_Waitall(2, req, NULL);
       printf("%d\n", world_rank);
     }
   }
   // правый нижний угол
-  else if (world_rank == MATRIX_N * MATRIX_N - 1) 
+  else if (world_rank == MATRIX_N * MATRIX_N - 1)
   {
     char msg[MSG_LENGTH];
-    for (int i = 0; i < NUM_CHUNKS; i++)
+    for (int i = 0; i < NUM_CHUNKS / 2; i++)
     {
-      MPI_Recv(msg + i * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank - MATRIX_N, i, MPI_COMM_WORLD, NULL);
+      MPI_Irecv(msg + i * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank - MATRIX_N, i, MPI_COMM_WORLD, NULL, req[0]);
+      MPI_Irecv(msg + (i + NUM_CHUNKS / 2) * CHUNK_LENGTH, CHUNK_LENGTH, MPI_BYTE, world_rank - 1, (i + NUM_CHUNKS / 2), MPI_COMM_WORLD, NULL, req[1]);
+      MPI_Waitall(2, req, NULL);
       printf("%d\n", world_rank);
     }
     printf("DONE!\n");
@@ -48,20 +52,38 @@ int main(int argc, char **argv)
     int source;
     int destination;
 
+    // правый верхний угол
+    if (world_rank == MATRIX_N - 1)
+    {
+      source = world_rank - 1;
+      destination = world_rank + MATRIX_N;
+    }
+    // левый нижний угол
+    else if (world_rank % MATRIX_N == 0)
+    {
+      source = world_rank - MATRIX_N;
+      destination = world_rank + 1;
+    }
     // верхняя горизонталь
     if (world_rank < MATRIX_N - 1)
     {
       source = world_rank - 1;
       destination = world_rank + 1;
     }
-    // правый верхний угол
-    else if (world_rank == MATRIX_N - 1)
+    // нижняя горизонталь
+    if (world_rank > MATRIX_N * (MATRIX_N - 1) && world_rank < MATRIX_N * MATRIX_N - 1)
     {
       source = world_rank - 1;
-      destination = world_rank + MATRIX_N;
+      destination = world_rank + 1;
     }
     // правая вертикаль
     else if (world_rank % MATRIX_N == MATRIX_N - 1)
+    {
+      source = world_rank - MATRIX_N;
+      destination = world_rank + MATRIX_N;
+    }
+    // левая вертикаль
+    else if (world_rank % MATRIX_N == 0)
     {
       source = world_rank - MATRIX_N;
       destination = world_rank + MATRIX_N;
@@ -85,8 +107,8 @@ int main(int argc, char **argv)
       else
       {
         MPI_Isend(chunk + CHUNK_LENGTH * ((i - 1) % 2), CHUNK_LENGTH, MPI_BYTE, destination, i - 1, MPI_COMM_WORLD, &req[0]);
-        MPI_Irecv(chunk + CHUNK_LENGTH * (i % 2), CHUNK_LENGTH, MPI_BYTE, source, i, MPI_COMM_WORLD, &req[1]);               
-        MPI_Waitall( 2 , req , NULL);
+        MPI_Irecv(chunk + CHUNK_LENGTH * (i % 2), CHUNK_LENGTH, MPI_BYTE, source, i, MPI_COMM_WORLD, &req[1]);
+        MPI_Waitall(2, req, NULL);
       }
       printf("%d\n", world_rank);
     }
